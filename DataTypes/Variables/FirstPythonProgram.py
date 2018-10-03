2667caa9e50d164ce30909f32f39b44fa4d6854e
rootsatya <rootsatya@gmail.com>
2018-10-04 00:49:17 +0530

    FirstGitCommit

---
+++ DataTypes/Variables/FirstPythonProgram.py
@@ -0,0 +1,14 @@
+#!/bin/usr/python
+print ("Start Your Python Programming")
+
+FirstName="Guido"
+MiddleName="Van"
+LastName="Rossum"
+Title="Python Creator"
+
+print(FirstName)
+print(MiddleName)
+print(LastName)
+print ("is")
+print(Title)
+
