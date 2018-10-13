7e377dff96754ac99f8b12c2dbe2174d46efe256
rootsatya <rootsatya@gmail.com>
2018-10-13 08:06:23 +0530

    modified:   .idea/workspace.xml 	new file:   DataTypes/Strings/String_Methods.py 	new file:   DataTypes/Strings/String_Special_Operators.py 	modified:   DataTypes/Variables/FirstPythonProgram.py

---
+++ DataTypes/Strings/String_Methods.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+#Python String capitalize()	Converts first character to Capital Letter
+
+#Example - 1
+City = "this is Bangalore City"
+pin = "pin code of Bangalore"
+pin_code = "560066"
+Course = "i am learning Python from"
+Institute = "naresh I Technology"
+
+print (City.capitalize())
+print (pin.capitalize(), end=" is ")
+print(pin_code)
+print (Course.capitalize())
+print (Institute.capitalize())
--- DataTypes/Variables/FirstPythonProgram.py
+++ DataTypes/Variables/FirstPythonProgram.py
@@ -1,8 +1,8 @@
 #!/bin/usr/python
-print ("Start Your Python Programming")
+print ("Variables and Printing")
 
-#Comments Section
-"this is my 1st python program"
+#1st Comments Section
+""
 FirstName="Guido"
 MiddleName="Van"
 LastName="Rossum"
@@ -14,3 +14,18 @@ print(LastName)
 print ("is")
 print(Title)
 
+#2nd Python Program
+print('2nd Program')
+firstname='Guido'
+middlename="Van"
+lastname='Rossum'
+title=''' Python Creator'''
+print('')
+print(firstname)
+print(middlename)
+print(lastname)
+print('is')
+print(title)
+print('')
+
+#3rd Programs
