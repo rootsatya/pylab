472d60099d51c57ddc53515c8d0e81a68ae43d33
rootsatya <rootsatya@gmail.com>
2018-10-13 16:11:21 +0530

    1st program with 5 examples

--- DataTypes/Variables/FirstPythonProgram.py
+++ DataTypes/Variables/FirstPythonProgram.py
@@ -1,8 +1,11 @@
 #!/bin/usr/python
-print ("Variables and Printing")
+
+#Comments Section
+#Comments can be created with #Sometext,'Sometext',''Sometext'',"""Sometext"""
+print("Variables and Printing")
 
 #1st Comments Section
-""
+"This is a 1st comment"
 FirstName="Guido"
 MiddleName="Van"
 LastName="Rossum"
@@ -19,7 +22,7 @@ print('2nd Program')
 firstname='Guido'
 middlename="Van"
 lastname='Rossum'
-title=''' Python Creator'''
+title="Python Creator"
 print('')
 print(firstname)
 print(middlename)
@@ -28,4 +31,46 @@ print('is')
 print(title)
 print('')
 
-#3rd Programs
+
+#3rd Program
+Country='India'
+State="Karnataka"
+City='''Bangalore'''
+Area= 'Whitefield'
+Pincode="""560066"""
+
+#Accessing Veriables
+print('Country : ',Country)
+print('State : ',State)
+print("City : ",City)
+print("Area : ",Area)
+print('Pincode : ',Pincode)
+#Printing all variables in single line with single print function
+print(Country,type(Country),id(Country), State,type(State),id(State),City,type(City),id(City),Area,type(Area),id(Area),Pincode,type(Pincode),id(Pincode))
+
+#4th Program
+Movie='AntMan'
+Director = '''Peyton Reed'''
+Producer = "Joe Cornish"
+Story = """Edgar Wright"""
+Music="""Chris Beck"""
+
+print(Movie,Director,Producer,Story,Music)
+
+#5th Program
+print("5th Program")
+"""This is multi line comment for 5th program and next line
+starts for the sake of multiline"""
+Language = 'Python 3.6'
+OS = '''Windows 7 Ent'''
+Text_Editor = "Sublime"
+IDLE = """PyCharm"""
+
+print(IDLE,Text_Editor,OS,Language)
+print(id(OS),id(Language),id(Text_Editor),id(IDLE))
+print(type(OS),type(Language),type(Text_Editor),type(IDLE))
+
+
+
+
+'''End of the Program'''
